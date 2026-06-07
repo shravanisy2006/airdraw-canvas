c3a52eeb9cb0775bc3ba1f6f1cb5497605a307fb
shravanisy2006 <shravanisy2000@gmail.com>
2026-06-07 19:02:50 +0530

    Index finger identification and points in pixels

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ while True:
 
     ret, frame = cam.read()
 
+    height , width , channels = frame.shape
+
     rgb_frame = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
 
     results = hands.process(rgb_frame)
@@ -20,17 +22,20 @@ while True:
     if results.multi_hand_landmarks:
 
         for hand_landmarks in results.multi_hand_landmarks:
+            index_finger = hand_landmarks.landmark[8]
+
+            x = int(index_finger.x *width)
+            y = int(index_finger.y * height)
+            
+            cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
+
             mp_draw.draw_landmarks(
                 frame,
                 hand_landmarks,
                 mp_hands.HAND_CONNECTIONS
             )
 
-            cv2.line(frame,(100,100),(300,100),(255,0,0),30)
-            cv2.rectangle(frame,(100,100),(300,200),(0,255,255),10)
-            cv2.circle(frame,(150,150),50,(0,0,255),3)
-
-            cv2.putText(frame,"Air Canvas- Day 2",(50,50),cv2.FONT_HERSHEY_COMPLEX,1.5,(0,0,255),4)
+            cv2.putText(frame,"Air Canvas",(50,50),cv2.FONT_HERSHEY_COMPLEX,1.5,(0,0,255),2)
 
     cv2.imshow("webcam",frame)
     if cv2.waitKey(1) == 27:
